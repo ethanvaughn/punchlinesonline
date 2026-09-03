8448517780159c1cbdb84adf5cfc1e2cd2dbd6f2
Ethan Vaughn <ethan.vaughn@gmail.com>
2026-09-03 20:31:30 +0000

    Add sql files to insert dev data.

---
+++ SQL Dev Data/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, email, first_name, last_name, password) VALUES
+    ('users-EvZyNFjBRmZhupSTTWBGRvx', 'ethan.vaughn@gmail.com', 'Ethan', 'Vaughn', '$2b$12$vNkYZLsBruULWYNFID1W9OYlTe7jRV1aNeiSnDxH4EkpuSyddCDfi')
+;
